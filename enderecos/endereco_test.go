034bc79475a02ceaf6ce344529197499b5a2b32d
Macedo <cleitonrt3@gmail.com>
2021-07-30 20:54:06 -0300

    refatorando o código do test e passando as váriaveis de test por slice e eniviando e recebendo por um struct

--- enderecos/endereco_test.go
+++ enderecos/endereco_test.go
@@ -2,12 +2,29 @@ package enderecos
 
 import "testing"
 
+type cenarioTeste struct {
+	enderecoInserido string
+	retornoEsperado  string
+}
+
 func TestTiposDeEnderecos(t *testing.T) {
-	enderecoParaTest := "Estrada dos bandeirantes"
-	tipoDeEnderecoEsperado := "Estrada"
-	tipoDeEnderecoRecebido := TiposDeEnderecos(enderecoParaTest)
-	if tipoDeEnderecoRecebido != tipoDeEnderecoEsperado {
-		t.Errorf("O tipo recebido é diferente do esperado! Esperava %s e recebeu %s",
-			tipoDeEnderecoEsperado, tipoDeEnderecoRecebido)
+	cenariosDeTeste := []cenarioTeste{
+		{"Rua Abc", "Rua"},
+		{"Avenida das Hortas", "Avenida"},
+		{"Rodovia dos Bandeirantes", "Rodovia"},
+		{"Estrada do Matagal", "Estrada"},
+		{"AVENIDA ESTREITA", "Avenida"},
+		{"", "Tipo de Endereço inválido!"},
+		{"Praça dos Bandeirantes", "Tipo de Endereço inválido!"},
+		{"rua do jaumzinhu", "Rua"},
+		{"av2ida do Grau", "Estrada"},
+	}
+
+	for _, cenario := range cenariosDeTeste {
+		retornoRecebido := TiposDeEnderecos(cenario.enderecoInserido)
+		if cenario.retornoEsperado != retornoRecebido {
+			t.Errorf("O tipo de Endereço retornado está inválido! Esperava receber %s recebeu %s",
+				cenario.retornoEsperado, retornoRecebido)
+		}
 	}
 }
